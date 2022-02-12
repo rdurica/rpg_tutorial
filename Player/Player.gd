3f1237041ef4a1ea960a6873c201689fb2de1a77
Robert Durica <r.durica@gmail.com>
2022-02-12 21:15:32 +0100

    Tutorial#9

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,15 +4,32 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum {
+    MOVE,
+    ROLL,
+    ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
+func _ready():
+    animation_tree.active = true
 
 func _physics_process(delta):
+    match state:
+        MOVE:
+            move_state(delta)
+        ROLL:
+            pass
+        ATTACK:
+            attack_state(delta)
+
+func move_state(delta):
     var input_vector = Vector2.ZERO
-    
     input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
     input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
     input_vector = input_vector.normalized()
@@ -20,6 +37,7 @@ func _physics_process(delta):
     if input_vector != Vector2.ZERO:
         animation_tree.set("parameters/idle/blend_position", input_vector)
         animation_tree.set("parameters/run/blend_position", input_vector)
+        animation_tree.set("parameters/attack/blend_position", input_vector)
         
         animation_state.travel("run")
         velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -28,3 +46,14 @@ func _physics_process(delta):
         velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
     
     velocity = move_and_slide(velocity)
+    
+    if Input.is_action_just_pressed("attack"):
+        state = ATTACK
+    
+# warning-ignore:unused_argument
+func attack_state(delta):
+    velocity = Vector2.ZERO
+    animation_state.travel("attack")
+
+func attack_animation_finished():
+    state = MOVE
